6868ae7dd010363f7df37909651856ab0e50a4f6
David J. Brenes <davidjbrenes@gmail.com>
2011-03-15 21:33:01 +0100

    #FIX: In CouchDB 1.0.1 all requests must specify the content-type header

--- lib/em-couchdb/couch_protocol.rb
+++ lib/em-couchdb/couch_protocol.rb
@@ -20,7 +20,7 @@ module EventMachine
       # DB API
 
       def get_all_dbs(&callback)
-        http = EventMachine::HttpRequest.new("http://#{@host}:#{@port}/_all_dbs/").get :timeout => @timeout 
+        http = EventMachine::HttpRequest.new("http://#{@host}:#{@port}/_all_dbs/").get :timeout => @timeout, :head => {"Content-Type" => "application/json"}
         http.callback {
           callback.call(JSON.load(http.response))
         }
@@ -30,7 +30,7 @@ module EventMachine
       end
 
       def create_db(db_name, &callback)
-        http = EventMachine::HttpRequest.new("http://#{@host}:#{@port}/#{db_name}/").put 
+        http = EventMachine::HttpRequest.new("http://#{@host}:#{@port}/#{db_name}/").put :head => {"Content-Type" => "application/json"}
         if block_given?
           http.callback {
             callback.call()
@@ -42,7 +42,7 @@ module EventMachine
       end
       
       def get_db(db_name, &callback)
-        http = EventMachine::HttpRequest.new("http://#{@host}:#{@port}/#{db_name}/").get :timeout => @timeout
+        http = EventMachine::HttpRequest.new("http://#{@host}:#{@port}/#{db_name}/").get :timeout => @timeout, :head => {"Content-Type" => "application/json"}
         http.callback {
           callback.call(JSON.load(http.response))
         }
@@ -52,7 +52,7 @@ module EventMachine
       end
       
       def delete_db(db_name, &callback)
-        http = EventMachine::HttpRequest.new("http://#{@host}:#{@port}/#{db_name}/").delete 
+        http = EventMachine::HttpRequest.new("http://#{@host}:#{@port}/#{db_name}/").delete :head => {"Content-Type" => "application/json"}
         if block_given?
           http.callback {
             callback.call()
@@ -78,7 +78,7 @@ module EventMachine
       # Document API
 
       def get(database, id, &callback)
-        http = EventMachine::HttpRequest.new("http://#{@host}:#{@port}/#{database}/#{id}").get :timeout => @timeout 
+        http = EventMachine::HttpRequest.new("http://#{@host}:#{@port}/#{database}/#{id}").get :timeout => @timeout, :head => {"Content-Type" => "application/json"} 
         http.callback {
           callback.call(JSON.load(http.response))
         }
@@ -99,7 +99,7 @@ module EventMachine
         id, rev = get_id_and_revision(old_doc)
         new_doc["_rev"] = rev
         new_doc["_id"] = id
-        http = EventMachine::HttpRequest.new("http://#{@host}:#{@port}/#{database}/#{id}").put :body => JSON.dump(new_doc)
+        http = EventMachine::HttpRequest.new("http://#{@host}:#{@port}/#{database}/#{id}").put :body => JSON.dump(new_doc), :head => {"Content-Type" => "application/json"}
         http.callback {
           callback.call(JSON.load(http.response))
         }
@@ -109,7 +109,7 @@ module EventMachine
       end
       def delete(database, doc, &callback)
         doc_id, doc_revision = get_id_and_revision(doc)
-        http = EventMachine::HttpRequest.new(URI.parse("http://#{@host}:#{@port}/#{database}/#{doc_id}?rev=#{doc_revision}")).delete
+        http = EventMachine::HttpRequest.new(URI.parse("http://#{@host}:#{@port}/#{database}/#{doc_id}?rev=#{doc_revision}")).delete :head => {"Content-Type" => "application/json"}
         http.callback{
           callback.call
         }
